b42d7483060e7c2fa60c8d9df768da0d16aad7ab
guigur <guigur13@gmail.com>
2017-05-28 16:42:43 +0200

    added buzzer & feature update

--- buzzer.ino
+++ buzzer.ino
@@ -4,7 +4,8 @@ void beep(unsigned char delayms)
 {
   if(silentMode == false && EEPROM.read(3) == 0)
   {
-    analogWrite(buzzer, 100);
+    tone(buzzer, 440, delayms);
+    //analogWrite(buzzer, 100);
     delay(delayms);
     analogWrite(buzzer, 0);
     delay(delayms);
